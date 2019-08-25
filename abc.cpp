99dd47a68bbfc07599057fefb00af1f39623cb3a
maxjo358 <maxjo358@student.liu.se>
2019-08-25 15:26:16 +0200

    Insertion sort now has its own file to live in

--- abc.cpp
+++ abc.cpp
@@ -1,35 +1,9 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include "insertion_sort.h"
 
 using namespace std;
-
-// Implementing insertion sort for fun
-template<typename T, size_t SIZE>
-void insertion_sort(array<T, SIZE>& arr)
-{
-	for (size_t sorted_index = 1; sorted_index < SIZE; sorted_index++)
-	{
-		T curr = arr[sorted_index];
-		// Find correct position in sub list
-		for (size_t sub_index = 0; sub_index < sorted_index; sub_index++)
-		{
-			if (arr[sub_index] > curr)
-			{
-				T tmp_last = arr[sub_index];
-				arr[sub_index] = curr;
-				// Shift remaning elements in sub list up
-				for (size_t i = sub_index + 1; i <= sorted_index; i++)
-				{
-					T tmp_next = arr[i];
-					arr[i] = tmp_last;
-					tmp_last = tmp_next;
-				}
-				break; // Sub list sorting done
-			}
-		}
-	}
-}
 
 // abc solution
 int main()
